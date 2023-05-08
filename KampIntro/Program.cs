8dec81b5e0667b838a67f0b370d5ea30547f5c2c
yunus <yunus@DESKTOP-UD9Q32E>
2023-05-09 02:35:40 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            // do not repeat yourself 
+
+             string kategoriEtiketi = "Kategoriler";
+
+            
+
+            bool sistemeGirişYapmişmi = false;
+
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("azalış butonu");
+
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("azalış butonu");
+            }
+            else {
+                Console.Write("değimedi butonu");
+            }
+
+          
+            if (sistemeGirişYapmişmi== true)
+            {
+                Console.WriteLine("ayarlar butonu");
+
+            }
+            else
+            {
+                Console.WriteLine("giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+            
+        }
+    }
+}
---
+++ Metodlar/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodlar
+{
+    class SepetManager
+    {
+        // naming convention
+        // syntax
+        public void Ekle(Urun urun)
+        {
+
+            Console.WriteLine("Tebrikler. Sepete Eklendi: " + urun.Adi);
+
+        }
+
+    }
+}
